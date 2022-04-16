52b4d1dda948fcb72278e5227ec335b26ed49ee1
Sergio Rodrigues dos Santos <sergiotinfo3@gmail.com>
2022-04-15 21:15:45 -0300

    ProjetoJavaScript

---
+++ function.js
@@ -0,0 +1,25 @@
+function mostrarOi()
+{
+    idade = 36;
+    idadePai = 78;
+    soma = idade + idadePai;
+
+    alert("Minha idade é : " + idade);
+    alert("Idade do meu pai é: " + idadePai);
+    alert("A soma da minha idade e a do meu pai é: " + soma);
+
+
+    //=========================================================
+    //alert("A soma das idades é: " + idade + idadePai);
+    //document.write( idade + idadePai);
+
+    /*if (idade >= 18)
+    {
+        alert("Minha idade é: ");
+    }
+    else
+    {
+        alert("A idade do meu pai é:");
+    }*/
+
+}
